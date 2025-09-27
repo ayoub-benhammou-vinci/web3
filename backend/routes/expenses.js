5eabbfd549cde1a6570b3c13b4e9eab93a7e19f6
Ayoub <ayoub.benhammou@student.vinci.be>
2025-09-28 01:16:30 +0200

    feat: add ex 2.5

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -1,46 +1,56 @@
-const express = require('express')
-const { getAllExpenses, addExpense, resetExpenses } = require('../services/expenses')
+const express = require("express");
+const {
+  getAllExpenses,
+  addExpense,
+  resetExpenses,
+} = require("../services/expenses");
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-    try {
-        const expenses = getAllExpenses();
-        res.json(expenses);
-    } catch (err) {
-        console.error("Error 500 ReadFileSync");
-        res.status(500);
-    }
-})
+router.get("/", async (_req, res) => {
+  try {
+    const expenses = await getAllExpenses();
+    res.json(expenses);
+  } catch (err) {
+    console.error("Error 500 ReadFileSync");
+    res.status(500);
+  }
+});
 
-router.post('/', (req, res) => {
-    if (req.body.id == undefined ||
-         req.body.date == undefined ||
-          req.body.description == undefined ||
-           req.body.payer == undefined ||
-            req.body.amount == undefined ||
-             req.body.amount <= 0) {
-                return res.status(400).json({ error: "Invalid expense data"});
-            }
+router.post("/", async (req, res) => {
+  if (
+    req.body.date == undefined ||
+    req.body.description == undefined ||
+    req.body.payer == undefined ||
+    req.body.amount == undefined ||
+    req.body.amount <= 0
+  ) {
+    return res.status(400).json({ error: "Invalid expense data" });
+  }
 
-    const newExpense = req.body;
+  const newExpense = {
+    date: req.body.date,
+    description: req.body.description,
+    payer: req.body.payer,
+    amount: parseFloat(req.body.amount),
+  }
 
-    try {
-        const data = addExpense(newExpense);
-        res.json(data);
-    } catch (err) {
-        console.error("Error 500 ReadFileSync and/or writeFileSync");
-        res.status(500);
-    }
-})
+  try {
+    const data = await addExpense(newExpense);
+    res.json(data);
+  } catch (err) {
+    console.error("Error 500 ReadFileSync and/or writeFileSync");
+    res.status(500);
+  }
+});
 
-router.post('/reset', (_req, res) => {
-    try {
-        const data = resetExpenses();
-        res.json(data);
-    } catch (err) {
-        console.error("Error 500 ReadFileSync and/or writeFileSync");
-        res.status(500);
-    }
-})
+router.post("/reset", async (_req, res) => {
+  try {
+    const data = await resetExpenses();
+    res.json(data);
+  } catch (err) {
+    console.error("Error 500 ReadFileSync and/or writeFileSync");
+    res.status(500);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- backend/services/expenses.js
+++ backend/services/expenses.js
@@ -1,27 +1,37 @@
-const fs = require("fs");
-const path = require('path');
+const { PrismaClient } = require('../generated/prisma');
 
-const EXPENSES_FILE_PATH = path.join(__dirname, '../data/expenses.json');
-const EXPENSES_DEFAULT = path.join(__dirname, '../data/expenses.init.json');
+const prisma = new PrismaClient();
+
+const queryDatabaseAndDisconnect = (operation) => {
+    return operation()
+        .finally(() => prisma.$disconnect)
+        .catch((e) => console.error(e));
+}
 
 const getAllExpenses = () => {
-    const data = fs.readFileSync(EXPENSES_FILE_PATH, 'utf8');
-    return JSON.parse(data);
+    const operation = () => prisma.expense.findMany();
+    const expenses = queryDatabaseAndDisconnect(operation);
+    return expenses;
 }
 
 const addExpense = (expense) => {
-    const expenses = getAllExpenses();
-    expenses.push(expense);
-
-    const updatedExpenses = JSON.stringify(expenses, null, 2);
-    fs.writeFileSync(EXPENSES_FILE_PATH, updatedExpenses);
-    return JSON.parse(updatedExpenses);
+    const operation = () => prisma.expense.create({data: expense});
+    return queryDatabaseAndDisconnect(operation);
 }
 
+const INITIAL_DATA = [
+  { date: '2025-01-16T00:00:00Z', description: 'Example expense #1 from Alice', payer: 'Alice', amount: 25.5 },
+  { date: '2025-01-15T00:00:00Z', description: 'Example expense #2 from Bob', payer: 'Bob', amount: 35 },
+  { date: '2025-01-15T00:00:00Z', description: 'Example expense #3 from Alice', payer: 'Alice', amount: 2 },
+];
+
 const resetExpenses = () => {
-    const data = fs.readFileSync(EXPENSES_DEFAULT, 'utf-8');
-    fs.writeFileSync(EXPENSES_FILE_PATH, data);
-    return JSON.parse(data);
+    const operation = async () => {
+        await prisma.expense.deleteMany({});
+        await prisma.expense.createMany({data: INITIAL_DATA});
+        return prisma.expense.findMany(); //Ca renvoie une promesse !
+    }
+    return queryDatabaseAndDisconnect(operation);
 }
 
 module.exports = { getAllExpenses, addExpense, resetExpenses };
