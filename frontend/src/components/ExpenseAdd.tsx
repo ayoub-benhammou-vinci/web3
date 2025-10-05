05f065a0a673ecf2f329bb7f16c792098a8b7ebe
Ayoub <ayoub.benhammou@student.vinci.be>
2025-10-05 14:28:35 +0200

    feat: validation with zod

--- frontend/src/components/ExpenseAdd.tsx
+++ frontend/src/components/ExpenseAdd.tsx
@@ -1,18 +1,30 @@
-import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import type { ExpenseInput } from "../types/Expense";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type ExpenseAddProps = {
   handleAdd: (expense: ExpenseInput) => void;
 };
 const ExpenseAdd = ({ handleAdd }: ExpenseAddProps) => {
+  const expenseSchema = z.object({
+    payer: z.enum(["Bob", "Alice"], { message: "Payer must be Alice or Bob" }),
+    amount: z.number().positive({ message: "Amount must be positive" }),
+    description: z
+      .string()
+      .max(200, { message: "Description max length is 200" }),
+    date: z.string().nonempty({ message: "Date is required" }),
+  });
+
+  type ExpenseFormData = z.infer<typeof expenseSchema>;
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ExpenseInput>();
-
-  useEffect(() => {}, []);
+  } = useForm<ExpenseFormData>({
+    resolver: zodResolver(expenseSchema),
+  });
 
   const onSubmit = ({ payer, amount, description, date }: ExpenseInput) => {
     const expense: ExpenseInput = {
@@ -31,38 +43,46 @@ const ExpenseAdd = ({ handleAdd }: ExpenseAddProps) => {
         style={{ display: "flex", flexDirection: "column", gap: "16px" }}
       >
         <label>
-          Description
+          Description :
           <input
-            {...register("description", { required: true })}
+            {...register("description")}
             placeholder="Description"
+            style={{ width: "400px" }}
           />
-          {errors.description && <span>Description field is required</span>}
+          {errors.description && (
+            <span style={{ color: "red" }}>{errors.description.message}</span>
+          )}
         </label>
 
-        <select {...register("payer", {required: true})}>
-          <option value="Bob">Bob</option>
-          <option value="Alice">Alice</option>
-        </select>
-        {errors.payer && <span>Payer is required</span>}
+        <label>
+          Selection :
+          <select {...register("payer")}>
+            <option value="Bob">Bob</option>
+            <option value="Alice">Alice</option>
+          </select>
+          {errors.payer && (
+            <span style={{ color: "red" }}>{errors.payer.message}</span>
+          )}
+        </label>
 
         <label>
-          Amount
+          Amount :
           <input
             type="number"
-            {...register("amount", { required: true })}
+            {...register("amount", { valueAsNumber: true })}
             placeholder="Amount"
           />
-          {errors.amount && <span> Amount field is required</span>}
+          {errors.amount && (
+            <span style={{ color: "red" }}>{errors.amount.message}</span>
+          )}
         </label>
 
         <label>
-          Date
-          <input
-            type="date"
-            {...register("date", { required: true })}
-            placeholder="Date"
-          />
-          {errors.date && <span>Date field is required</span>}
+          Date :
+          <input type="date" {...register("date")} placeholder="Date" />
+          {errors.date && (
+            <span style={{ color: "red" }}>{errors.date.message}</span>
+          )}
         </label>
         <button type="submit">Add</button>
       </form>
